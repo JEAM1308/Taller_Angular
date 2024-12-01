abf157fd0810325bb875f5596e587060b0b3dbf5
Juan Arboleda <j.arboledam@uniandes.edu.co>
2024-12-01 12:28:22 -0500

    +

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SerieModule  } from './serie/serie.module';
+import { ProfesorModule } from './profesor/profesor.module'
 
 @NgModule({
   declarations: [
@@ -12,7 +12,7 @@ import { SerieModule  } from './serie/serie.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SerieModule
+    ProfesorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/profesor/profesor-list/profesor-list.component.ts
+++ src/app/profesor/profesor-list/profesor-list.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-profesor-list',
   templateUrl: './profesor-list.component.html',
-  styleUrls: ['./profesor-list.component.css']
+  styleUrls: ['./profesor-list.component.css'],
+  standalone: false
 })
 export class ProfesorListComponent implements OnInit {
 
